019783138f8883028269195c5a0940d527081d58
Kiran Kumar Bomburi <84639514+DarkAvenger10000@users.noreply.github.com>
2026-02-04 00:56:18 +0530

    Update RelatedProducts.jsx

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -17,7 +17,7 @@ const RelatedProducts = ({category,id}) => {
       body: JSON.stringify({category:category}),
       })
     .then((res)=>res.json()).then((data)=>setRelated(data))
-  },[])
+  },[category])
 
   return (
     <div className='relatedproducts'>
